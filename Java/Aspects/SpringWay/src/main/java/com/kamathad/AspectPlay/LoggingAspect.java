85c514816963a0554faf3ac4da4e7e5f9013cd37
Adithya Kamath <adithyakamath96@gmail.com>
2025-02-03 02:34:21 +0530

    setting aspect j at compile time

---
+++ Java/Aspects/SpringWay/src/main/java/com/kamathad/AspectPlay/LoggingAspect.java
@@ -0,0 +1,12 @@
+package com.kamathad.AspectPlay;
+
+import org.springframework.stereotype.Component;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+
+@Aspect
+@Component
+public class LoggingAspect {
+}
---
+++ Java/Aspects/SpringWay/src/test/java/com/kamathad/AspectPlay/AspectPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamathad.AspectPlay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspectPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
