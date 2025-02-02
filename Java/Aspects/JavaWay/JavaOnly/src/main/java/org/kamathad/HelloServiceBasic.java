402bea4e9f521201efdbfe4bc342c9b41325fd1d
Adithya Kamath <adithyakamath96@gmail.com>
2025-02-03 02:09:21 +0530

    a basic hello

---
+++ Java/Aspects/JavaWay/JavaOnly/src/main/java/org/kamathad/HelloServiceBasic.java
@@ -0,0 +1,10 @@
+package org.kamathad;
+
+public class HelloServiceBasic {
+
+    public void sayHello(String name) {
+        System.out.println("Method sayHello() started");
+        System.out.println("Hello, "+name+"!");
+        System.out.println("Method sayHello() execution completed");
+    }
+}
