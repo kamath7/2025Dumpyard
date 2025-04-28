e670a40edc85c96cd222fc583d6d492c1b6fc48c
Adithya Kamath <adithyakamath96@gmail.com>
2025-04-29 01:08:41 +0530

    Model created

---
+++ Python/salary_predictor/train_model.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+dataset = pd.read_csv("./Salary_dataset.csv")
+
+X = dataset[["YearsExperience"]].values 
+y = dataset["Salary"].values
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=0)
+
+regression = LinearRegression()
+regression.fit(X_train, y_train)
+
+print(regression.predict([[14]]))
+
