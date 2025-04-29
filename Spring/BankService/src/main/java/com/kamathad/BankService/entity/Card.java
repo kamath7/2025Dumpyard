eaaa8226b72a6c11f8c95a03e9fc5a752620a882
Adithya <adithyakamath96@gmail.com>
2025-04-30 02:18:25 +0530

    Card entity setup

--- Spring/BankService/src/main/java/com/kamathad/BankService/entity/Card.java
+++ Spring/BankService/src/main/java/com/kamathad/BankService/entity/Card.java
@@ -1,4 +1,71 @@
 package com.kamathad.BankService.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
 public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String cardNumber;
+    private LocalDate issueDate;
+
+    @ManyToOne
+    @JoinColumn(name="account_id")
+    private Account account;
+
+    public Card() {
+    }
+
+    public Card(String cardNumber, LocalDate issueDate, Account account) {
+        this.cardNumber = cardNumber;
+        this.issueDate = issueDate;
+        this.account = account;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public LocalDate getIssueDate() {
+        return issueDate;
+    }
+
+    public void setIssueDate(LocalDate issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "id=" + id +
+                ", cardNumber='" + cardNumber + '\'' +
+                ", issueDate=" + issueDate +
+                ", account=" + account +
+                '}';
+    }
 }
