fc8e6a50f28a5b594eb72df8b825db690c160e9b
Adithya Kamath <adithyakamath96@gmail.com>
2025-07-01 00:04:12 +0530

    minor dep fixes

--- Java/MockitoPlayGround/Mockito5xExperiments/src/main/java/com/kamathad/Mockito5xExperiments/Mockito5xExperimentsApplication.java
+++ Java/MockitoPlayGround/Mockito5xExperiments/src/main/java/com/kamathad/Mockito5xExperiments/Mockito5xExperimentsApplication.java
@@ -2,12 +2,11 @@ package com.kamathad.Mockito5xExperiments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Mockito5xExperimentsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Mockito5xExperimentsApplication.class, args);
 	}
-
 }
---
+++ Java/MockitoPlayGround/Mockito5xExperiments/src/test/java/com/kamathad/Mockito5xExperiments/UserServiceTest.java
@@ -0,0 +1,7 @@
+package com.kamathad.Mockito5xExperiments;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+}
