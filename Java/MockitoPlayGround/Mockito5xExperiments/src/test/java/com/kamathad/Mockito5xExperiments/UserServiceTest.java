cfd989e0abcc8fb0dc2da10dde2da20de20fb7b4
Adithya Kamath <adithyakamath96@gmail.com>
2025-07-01 00:15:24 +0530

    Mockito-5.x

--- Java/MockitoPlayGround/Mockito5xExperiments/src/test/java/com/kamathad/Mockito5xExperiments/UserServiceTest.java
+++ Java/MockitoPlayGround/Mockito5xExperiments/src/test/java/com/kamathad/Mockito5xExperiments/UserServiceTest.java
@@ -1,7 +1,42 @@
 package com.kamathad.Mockito5xExperiments;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-class UserServiceTest {
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = AppConfig.class)
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.openMocks(this);
+        ReflectionTestUtils.setField(userService, "userRepository", userRepository);
+    }
+
+    @Test
+    public void testGetUserName() {
+        when(userRepository.findById(1L)).thenReturn(new User("Lalle"));
+        String name = userService.getUserName(1L);
+        assertEquals("Lalle", name);
+    }
 
 }
