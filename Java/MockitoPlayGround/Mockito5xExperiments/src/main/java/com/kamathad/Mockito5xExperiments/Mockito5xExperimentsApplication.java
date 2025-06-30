bf8224913a0fc1ef436bb0d04a7f877b39d2badf
Adithya Kamath <adithyakamath96@gmail.com>
2025-06-30 23:52:40 +0530

    Pom setup

---
+++ Java/MockitoPlayGround/Mockito5xExperiments/src/main/java/com/kamathad/Mockito5xExperiments/Mockito5xExperimentsApplication.java
@@ -0,0 +1,13 @@
+package com.kamathad.Mockito5xExperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mockito5xExperimentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mockito5xExperimentsApplication.class, args);
+	}
+
+}
---
+++ Java/MockitoPlayGround/Mockito5xExperiments/src/test/java/com/kamathad/Mockito5xExperiments/Mockito5xExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamathad.Mockito5xExperiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mockito5xExperimentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
