0d838fdecb08ae961280fbc18c4b163e86659494
Adithya Kamath <adithyakamath96@gmail.com>
2025-06-30 23:47:01 +0530

    Tests working for 1.x

--- Java/MockitoPlayGround/Mockito1xExperiments/src/test/java/com/kamathad/Mockito1xExperiments/UserServiceTest.java
+++ Java/MockitoPlayGround/Mockito1xExperiments/src/test/java/com/kamathad/Mockito1xExperiments/UserServiceTest.java
@@ -1,15 +1,26 @@
 package com.kamathad.Mockito1xExperiments;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@TestExecutionListeners(listeners = {
+        DependencyInjectionTestExecutionListener.class
+})
+
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = AppConfig.class)
 public class UserServiceTest {
 
@@ -21,6 +32,14 @@ public class UserServiceTest {
 
     @Before
     public void setup(){
+        MockitoAnnotations.initMocks(this);
+        ReflectionTestUtils.setField(userService, "userRepository", userRepository);
+    }
 
+    @Test
+    public void testGetUserName() {
+        when(userRepository.findById(1L)).thenReturn(new User("Lalle"));
+        String name = userService.getUserName(1L);
+        assertEquals("Lalle", name);
     }
 }
