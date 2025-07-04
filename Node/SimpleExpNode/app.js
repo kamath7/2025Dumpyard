226b346a4ed08916468aa584ec8438c0e5812be2
Adithya Kamath <adithyakamath96@gmail.com>
2025-07-05 01:09:30 +0530

    Creating views and what not

--- Node/SimpleExpNode/app.js
+++ Node/SimpleExpNode/app.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 
 const app = express();
 
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send({ message: "Hello there" });
+  res.render("home", {
+    title: "Hello there",
+    message: "Generated with love using Handlebars",
+  });
 });
 
 const port = process.env.PORT || 3000;
