8cd87ee7681cb03b64644209fcf9e626c8f3367f
Adithya Kamath <adithyakamath96@gmail.com>
2025-07-05 01:15:41 +0530

    Rendering users

--- Node/SimpleExpNode/app.js
+++ Node/SimpleExpNode/app.js
@@ -15,6 +15,18 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/users", (req, res) => {
+  const users = [
+    { name: "Nata", age: 69 },
+    { name: "Seere", age: 45 },
+    { name: "Gokesh", age: 31 },
+  ];
+  res.render("users", {
+    title: "User List",
+    users,
+  });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("Server now running");
